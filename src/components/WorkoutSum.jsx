1e5916d1cf5df42f8611dcbe25d448955c31c211
SaraJohanssonn <sara.johanss0n@hotmail.com>
2024-05-13 23:14:18 +0200

    fix

--- src/components/WorkoutSum.jsx
+++ src/components/WorkoutSum.jsx
@@ -20,8 +20,7 @@ const WorkoutSum = () => {
   const formatTime = () => {
     const hours = Math.floor(time / 3600);
     const minutes = Math.floor((time % 3600) / 60);
-    const seconds = time % 60;
-    return `${hours}h ${minutes}m ${seconds}s`;
+    return `${hours}h ${minutes}m`;
   };
 
   return (
